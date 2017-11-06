4763ecd29392b49922d8538d2393b40cd42b8097
tapioka7 <tkojima703@gmail.com>
2017-11-06 13:07:28 +0900

    Add: upload file

--- web.py
+++ web.py
@@ -1,11 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, send_from_directory
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/result', methods=['GET','POST'])
+def result():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            result = '/uploads/' + filename
+            return render_template('result.html', result=result)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run()
